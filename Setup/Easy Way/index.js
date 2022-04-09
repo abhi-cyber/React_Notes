dbd07656c9974f5196caac9e61fe0f735007a2eb
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-09 15:59:58 +0530

    Rendring a list

--- Setup/Easy Way/index.js
+++ Setup/Easy Way/index.js
@@ -1,3 +1,8 @@
 //  React renders HTML to the web page by using a function called render().
 // The purpose of the function is to display the specified HTML code inside the specified HTML element.
+
 ReactDOM.render(<h1>Hello, everyone!!</h1>, document.getElementById("root"))
+
+// Another example
+
+// ReactDOM.render(<ul><li>Chitkara</li><li>BITs</li><li>NIT Delhi</li></ul>, document.getElementById("root"))
