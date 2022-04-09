37fa13b0a273ec2f20a36f5e0dd3e0e6f5a62d42
abhi-cyber <abhirajchatrath@gmail.com>
2022-04-09 15:55:49 +0530

    render() func.

---
+++ Setup/Easy Way/index.js
@@ -0,0 +1,3 @@
+//  React renders HTML to the web page by using a function called render().
+// The purpose of the function is to display the specified HTML code inside the specified HTML element.
+ReactDOM.render(<h1>Hello, everyone!!</h1>, document.getElementById("root"))
